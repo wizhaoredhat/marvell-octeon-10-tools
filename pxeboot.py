wizhaoredhat/marvell-octeon-10-tools

commit 20db07269d3084fb89df5180e2001b3a80855482

pxeboot: generate and install SSH key on DPU
On the host, if it doesn't exist generate a password less SSH key
"/root/.ssh/id_ed25519".

Then, via kickstart write the public key to "authorized_keys" inside the
DPU.

The purpose is to be able to SSH into the DPU.

pxeboot.py
@@ -4,12 +4,14 @@ import argparse
 import http.server
 import os
 import pexpect
+import shlex
 import shutil
 import signal
 import time
 
 from collections.abc import Iterable
 from multiprocessing import Process
+from typing import Optional
 
 import common_dpu
 
@@ -39,6 +41,11 @@ def parse_args() -> argparse.Namespace:
         type=str,
         default="/host",
         help="Optional argument where the host filesystem is mounted. Default is '/host'. Run podman with \"-v /:/host\".",
+    )
+    parser.add_argument(
+        "--ssh-key",
+        nargs="+",
+        help='Specify SSH public keys to add to the DPU\'s /root/.ssh/authorized_keys. Can be specified multiple times. If unspecified or set to "", include "/{host-path}/root/.ssh/id_ed25519.pub" (this file will be generated if it doesn\'t exist).',
     )
 
     args = parser.parse_args()
@@ -168,10 +175,23 @@ def http_server() -> None:
     httpd.serve_forever()
 
 
-def setup_http() -> None:
+def copy_kickstart(ssh_pubkey: list[str]) -> None:
+    with open(common_dpu.packaged_file("manifests/pxeboot/kickstart.ks"), "r") as f:
+        kickstart = f.read()
+
+    kickstart = kickstart.replace(
+        "@__SSH_PUBKEY__@", shlex.quote("\n".join(ssh_pubkey))
+    )
+
+    with open("/www/kickstart.ks", "w") as f:
+        f.write(kickstart)
+
+
+def setup_http(ssh_pubkey: list[str]) -> None:
     os.makedirs("/www", exist_ok=True)
     run(f"ln -s {iso_mount_path} /www")
-    shutil.copy(common_dpu.packaged_file("manifests/pxeboot/kickstart.ks"), "/www/")
+
+    copy_kickstart(ssh_pubkey)
 
     p = Process(target=http_server)
     p.start()
@@ -199,12 +219,30 @@ def setup_tftp() -> None:
     )
 
 
-def prepare_host(dev: str, host_path: str) -> None:
+def prepare_host(dev: str, host_path: str, ssh_key: Optional[list[str]]) -> list[str]:
     common_dpu.nmcli_setup_mngtiface(
         ifname=dev,
         chroot_path=host_path,
         ip4addr=common_dpu.host_ip4addrnet,
     )
+
+    ssh_pubkey = []
+
+    add_host_key = True
+    if ssh_key:
+        add_host_key = False
+        for s in ssh_key:
+            if not s:
+                add_host_key = True
+            else:
+                ssh_pubkey.append(s)
+
+    if add_host_key:
+        ssh_privkey_file = common_dpu.ssh_generate_key(host_path)
+        if ssh_privkey_file is not None:
+            ssh_pubkey.append(common_dpu.ssh_read_pubkey(ssh_privkey_file))
+
+    return ssh_pubkey
 
 
 def setup_dhcp() -> None:
@@ -226,11 +264,11 @@ def mount_iso(iso: str) -> None:
 
 
 def prepare_pxeboot(args: argparse.Namespace) -> None:
-    prepare_host(args.dev, args.host_path)
+    ssh_pubkey = prepare_host(args.dev, args.host_path, args.ssh_key)
     setup_dhcp()
     mount_iso(args.iso)
     setup_tftp()
-    setup_http()
+    setup_http(ssh_pubkey)
 
 
 def try_pxeboot(args: argparse.Namespace) -> None:

Containerfile
@@ -5,6 +5,7 @@ RUN dnf install -y 'dnf-command(config-manager)' && \
     dnf install -y epel-next-release epel-release && \
     dnf upgrade -y --skip-broken --allowerasing && \
     dnf install \
+        /usr/bin/ssh-keygen \
         dhcp-server \
         ethtool \
         iproute \

common_dpu.py
@@ -78,3 +78,28 @@ def nmcli_setup_mngtiface(
             die_on_error=True,
         )
     host.local.run(f"{chroot_prefix}nmcli connection up {con_spec}", die_on_error=True)
+
+
+def ssh_generate_key(chroot_path: str) -> str:
+    file = f"{chroot_path}/root/.ssh/id_ed25519"
+    if not os.path.exists(file) or not os.path.exists(f"{file}.pub"):
+        try:
+            os.mkdir(os.path.dirname(file))
+        except FileExistsError:
+            pass
+        host.local.run(
+            f'ssh-keygen -t ed25519 -C marvell-tools@local.local -N "" -f {shlex.quote(file)}',
+            die_on_error=True,
+        )
+    return file
+
+
+def ssh_read_pubkey(ssh_privkey_file: str) -> str:
+    ssh_pubkey_file = f"{ssh_privkey_file}.pub"
+    with open(ssh_pubkey_file, "r") as f:
+        ssh_pubkey = f.read()
+    for s in ssh_pubkey.splitlines():
+        s = s.strip()
+        if s:
+            return s
+    raise RuntimeError('failure to read SSH public key from "{ssh_pubkey_file}"')

manifests/pxeboot/kickstart.ks
@@ -53,3 +53,21 @@ grubby
 xterm
 NetworkManager-config-server
 %end
+
+################################################################################
+#
+################################################################################
+
+%post --log=/var/log/kickstart_post.log
+
+set -x
+
+SSH_PUBKEY=@__SSH_PUBKEY__@
+if [ -n "$SSH_PUBKEY" ] ; then
+    mkdir -p /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    printf '%s\n' "$SSH_PUBKEY" >> /root/.ssh/authorized_keys
+fi
+
+%end

commit 9585eaf7eb42153bbca503121c64df4e49e6225b

pxeboot: fix handling of "--ssh-key" option
As it was, only the last option wa considered. We want to allow
specifying "--ssh-key" multiple times, and to accept each value.

Fixes: 20db07269d30 ('pxeboot: generate and install SSH key on DPU')

pxeboot.py
@@ -47,7 +47,7 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--ssh-key",
-        nargs="+",
+        action="append",
         help='Specify SSH public keys to add to the DPU\'s /root/.ssh/authorized_keys. Can be specified multiple times. If unspecified or set to "", include "/{host-path}/root/.ssh/id_ed25519.pub" (this file will be generated with "--host-mode=rhel" if it doesn\'t exist).',
     )
     parser.add_argument(