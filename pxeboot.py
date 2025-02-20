wizhaoredhat/marvell-octeon-10-tools

commit d93ffb5b7ff421f2c968343276b69cbfe59cd78c

pxeboot: log generated SSH key

pxeboot.py
@@ -413,7 +413,14 @@ def prepare_host(
             create=True,
         )
         if ssh_privkey_file is not None:
+            logger.info(f"prepare-host: add host key {repr(ssh_privkey_file)}")
             ssh_pubkey.append(common_dpu.ssh_read_pubkey(ssh_privkey_file))
+
+    if ssh_pubkey:
+        logger.info("prepare-host: no SSH keys")
+    else:
+        for k in ssh_pubkey:
+            logger.info(f"prepare-host: use SSH key {repr(k)}")
 
     return ssh_pubkey
 

common_dpu.py
@@ -111,6 +111,7 @@ def ssh_generate_key(chroot_path: str, *, create: bool = True) -> Optional[str]:
     file = f"{chroot_path}/root/.ssh/id_ed25519"
     if not os.path.exists(file) or not os.path.exists(f"{file}.pub"):
         if not create:
+            logger.info(f"ssh-generate-key: skip creating key {repr(file)} on host")
             return None
         try:
             os.mkdir(os.path.dirname(file))
@@ -120,6 +121,9 @@ def ssh_generate_key(chroot_path: str, *, create: bool = True) -> Optional[str]:
             f'ssh-keygen -t ed25519 -C marvell-tools@local.local -N "" -f {shlex.quote(file)}',
             die_on_error=True,
         )
+        logger.info(f"ssh-generate-key: SSH key {repr(file)} created")
+    else:
+        logger.info(f"ssh-generate-key: use existing SSH key {repr(file)}")
     return file
 
 

commit 55f96484e75ca1cc6cf6c4f472f8a1d214af5528

pxeboot: fix logging SSH key
Fixes: d93ffb5b7ff4 ('pxeboot: log generated SSH key')

pxeboot.py
@@ -416,7 +416,7 @@ def prepare_host(
             logger.info(f"prepare-host: add host key {repr(ssh_privkey_file)}")
             ssh_pubkey.append(common_dpu.ssh_read_pubkey(ssh_privkey_file))
 
-    if ssh_pubkey:
+    if not ssh_pubkey:
         logger.info("prepare-host: no SSH keys")
     else:
         for k in ssh_pubkey: