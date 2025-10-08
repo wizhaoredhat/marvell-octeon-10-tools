wizhaoredhat/marvell-octeon-10-tools

commit d0c8af5830a6e9ac11bd172746e291da907a16fe

pxeboot: improve cleanup actions
When we are done, explicitly call common_dpu.global_cleanup.cleanup(),
instead of common.thread_list_join_all(). The benefit is that this
clears all cleanup actions that we have.

Otherwise, we log our "final" success message and then there come
more messages about the cleanup.

While at it, register the common.thread_list_join_all cleanup action
when constructing global_cleanup.

pxeboot.py
@@ -1247,7 +1247,7 @@ def main() -> None:
     post_pxeboot(ctx)
 
     logger.info("Terminating http, tftp, and dhcpd")
-    common.thread_list_join_all()
+    common_dpu.global_cleanup.cleanup()
 
     host_setup_only_msg = ""
     host_ips_msg = ""

common_dpu.py
@@ -32,7 +32,7 @@ TTYUSB0 = "/dev/ttyUSB0"
 TTYUSB1 = "/dev/ttyUSB1"
 
 
-global_cleanup = common.CleanupList()
+global_cleanup = common.CleanupList(common.thread_list_join_all)
 
 logger = common.ExtendedLogger("marvell_toolbox")
 
@@ -385,5 +385,4 @@ def ignition_storage_file(
 
 
 def run_main(main_fcn: Callable[[], None]) -> None:
-    global_cleanup.add(common.thread_list_join_all)
     common.run_main(main_fcn, cleanup=global_cleanup)

commit 6966f376721fc63d44899dd86c445baccba95c33

pxeboot: fix cleanup to fetch IP addresses first
Getting IP addresses via ssh_get_ipaddrs() requires SSH access, which
requires the SSH key.

On the other hand, cleanup will delete the internal SSH key.

We need to re-order the operations.

Fixes: d0c8af5830a6 ('pxeboot: improve cleanup actions')

pxeboot.py
@@ -1254,9 +1254,6 @@ def main() -> None:
 
     post_pxeboot(ctx)
 
-    logger.info("Terminating http, tftp, and dhcpd")
-    common_dpu.global_cleanup.cleanup()
-
     host_setup_only_msg = ""
     host_ips_msg = ""
 
@@ -1266,6 +1263,9 @@ def main() -> None:
         other_host_ips = ssh_get_ipaddrs(ctx, host_ip=host_ip)
         if other_host_ips:
             host_ips_msg = f" (or on {list(other_host_ips)}"
+
+    logger.info("Terminating http, tftp, and dhcpd")
+    common_dpu.global_cleanup.cleanup()
 
     logger.info(
         f"SUCCESS{host_setup_only_msg}. Try `ssh {ctx.iso_kind.SSH_USER}@{host_ip}`{host_ips_msg}"